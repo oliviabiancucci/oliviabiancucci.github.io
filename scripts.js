8c3439e8e71a9c977b04159a821cfb1f58debb2d
Olivia Biancucci <obiancuc@uoguelph.ca>
2024-04-28 11:34:07 -0400

    style changes

--- scripts.js
+++ scripts.js
@@ -1,30 +1,41 @@
+/*
+* Wanted to try out using Javascript to dynamically create HTML code from a JSON file.
+*/
 document.addEventListener("DOMContentLoaded", function() {
     fetch('./projects.json')
         .then(response => response.json())
         .then(data => {
             const projects = data.projects;
             const projectsContainer = document.querySelector('.list');
-            console.log('hello');
 
             projects.forEach(project => {
                 const panel = document.createElement('div');
                 panel.classList.add('panel');
 
-                const panelBody = document.createElement('div');
-                panelBody.classList.add('panel-body');
+                const textSection1 = document.createElement('div');
+                textSection1.classList.add('text-section');
 
                 const projectName = document.createElement('h5');
                 projectName.classList.add('display-inline');
                 projectName.textContent = project.projectName;
+
+                const textSection2 = document.createElement('div');
+                textSection2.classList.add('text-section');
 
                 const languages = document.createElement('div');
                 languages.classList.add('text-lead');
                 languages.classList.add('display-inline');
                 languages.textContent = 'Language(s): ' + project.languages;
 
+                const textSection3 = document.createElement('div');
+                textSection3.classList.add('text-section');
+
                 const description = document.createElement('div');
                 description.classList.add('text-lead');
                 description.textContent = project.description;
+
+                const textSection4 = document.createElement('div');
+                textSection4.classList.add('text-section');
 
                 const githubLink = document.createElement('a');
                 githubLink.classList.add('githubLink');
@@ -32,12 +43,17 @@ document.addEventListener("DOMContentLoaded", function() {
                 githubLink.href = project.github;
                 githubLink.target = '_blank';
 
-                panelBody.appendChild(projectName);
-                panelBody.appendChild(languages);
-                panelBody.appendChild(description);
-                panelBody.appendChild(githubLink);
+                panel.appendChild(textSection1);
+                textSection1.appendChild(projectName);
 
-                panel.appendChild(panelBody);
+                panel.appendChild(textSection2);
+                textSection2.appendChild(languages);
+
+                panel.appendChild(textSection3);
+                textSection3.appendChild(description);
+
+                panel.appendChild(textSection4);
+                textSection4.appendChild(githubLink);
 
                 projectsContainer.appendChild(panel);
             });
